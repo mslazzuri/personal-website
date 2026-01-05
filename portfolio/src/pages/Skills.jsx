66a00c71aa6609a10ec09ffead13f9231c0558b2
matheussecco <mslazzuri@mavs.coloradomesa.edu>
2026-01-05 17:25:24 -0500

    skills section bare minimum

--- portfolio/src/pages/Skills.jsx
+++ portfolio/src/pages/Skills.jsx
@@ -1,7 +1,46 @@
+import "../styles/Skills.css";
+import skills from '../skills.json';
+import { p } from "framer-motion/client";
+
 function Skills() {
+    const skillsData = skills[0];
+    const history = skills[1];
+
+    function formalize(text) {
+        let formalized = text[0].toUpperCase();
+        formalized += text.slice(1);
+        return formalized.replace(/[-_]/g, ' ');
+    }
+
     return (
         <>
-            My skills
+            <h3>skills</h3>
+            
+            <div className="skills-page-divided-pannel">
+                <div className="left-side">
+                    <div className="skills-sections">
+                        {Object.entries(skillsData).map(([sectionTitle, sectionSkills]) => (
+                            <div key={sectionTitle} className="skill-section">
+                                <div className='section-title'>{formalize(sectionTitle)}</div>
+                                <div 
+                                    className="skills-list"
+                                    style={{gridTemplateColumns: `repeat(${sectionSkills.length % 10}, 1fr)`}}
+                                >
+                                    {sectionSkills.map((skill, index) => (
+                                        skill && <p key={index} className="skill-item">{skill}</p>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="right-side">
+                    {
+                        Object.entries(history).map(([paragraph, text]) => (
+                            <p>{text}</p>
+                        ))}
+                </div>
+            </div> 
         </>
     );
 }
